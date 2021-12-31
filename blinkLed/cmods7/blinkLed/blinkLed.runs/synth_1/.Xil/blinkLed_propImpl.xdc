e4b4980848f0da478827239d71434c5b2fa324a2
a3333333 <alfredc13@yahoo.com>
2021-12-31 17:11:09 +0800

    Commit

---
+++ blinkLed/cmods7/blinkLed/blinkLed.runs/synth_1/.Xil/blinkLed_propImpl.xdc
@@ -0,0 +1,3 @@
+set_property SRC_FILE_INFO {cfile:C:/Xilinx/workspace/blinkLed/blinkLed.srcs/constrs_1/new/blinkLed.xdc rfile:../../../blinkLed.srcs/constrs_1/new/blinkLed.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:17 export:INPUT save:INPUT read:READ} [current_design]
+set_property -dict { PACKAGE_PIN F2    IOSTANDARD LVCMOS33 } [get_ports { redLed }]; #IO_L10P_T1_34 Sch=led0_r
---
+++ blinkLed/cmods7/blinkLed/blinkLed.srcs/sources_1/new/blinkLed.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 20.12.2021 18:25:14
+// Design Name: 
+// Module Name: blinkLed
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module blinkLed(
+    output redLed
+    );
+    
+  assign redLed = 0;
+  
+endmodule
